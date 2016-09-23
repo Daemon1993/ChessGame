cd58154c7bec019d67b4c3d4ba3b211a0c58b526
liubo <965204390@qq.com>
2016-09-23 18:54:45 +0800

    拿牌 数据越界

--- ServerProject/Utils.py
+++ ServerProject/Utils.py
@@ -5,7 +5,8 @@ import ServerProject.model.ResponseData
 #aaaaa
 def getRandomPuke(pukes):
     size=len(pukes)
-    index=random.randint(0,size)
+    index=random.randint(0,size-1)
+
 
     puke=pukes[index]
     pukes.remove(puke)
