d7f63170bad0a95489e895c136690498ff649189
liubo <965204390@qq.com>
2016-09-23 18:59:56 +0800

    拿到牌 随机优化

--- ServerProject/Utils.py
+++ ServerProject/Utils.py
@@ -4,11 +4,9 @@ import ServerProject.model.ResponseData
 
 #aaaaa
 def getRandomPuke(pukes):
-    size=len(pukes)
-    index=random.randint(0,size-1)
 
+    puke=random.choice(pukes)
 
-    puke=pukes[index]
     pukes.remove(puke)
     return puke
 
